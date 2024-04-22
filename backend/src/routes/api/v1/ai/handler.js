c594a0589dcbaf8feee64f4a3fccfac42df1536e
JohnHo112 <hokengseng0112@gmail.com>
2024-04-23 02:22:47 +0800

    ai log all

--- backend/src/routes/api/v1/ai/handler.js
+++ backend/src/routes/api/v1/ai/handler.js
@@ -2,22 +2,26 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import xss from 'xss'
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function rewrite(req, res) {
   try{
-    console.log(process.env.API_KEY);
+    console.log(process.env.GEMINI_API_KEY);
     const sentence = xss(req.body.sentence);
     console.log("rewrite sentence");
     console.log(sentence);
   
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
+    console.log(model);
+    
     const prompt = "Rewrite following sentence:\n" + sentence;
-  
+    console.log(prompt);
+    
     const result = await model.generateContent(prompt);
+    console.log(result);
     const response = await result.response;
+    console.log(response);
     const text = response.text();
     console.log("ai rewrite sentence");
     console.log(text);
