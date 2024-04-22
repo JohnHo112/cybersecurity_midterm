47eaf6059f77b4def8c4ae58e60428abbeab0619
JohnHo112 <hokengseng0112@gmail.com>
2024-04-23 02:47:10 +0800

    ai api problem solve

--- backend/src/routes/api/v1/ai/handler.js
+++ backend/src/routes/api/v1/ai/handler.js
@@ -5,25 +5,18 @@ import xss from 'xss'
 export async function rewrite(req, res) {
   // Access your API key as an environment variable (see "Set up your API key" above)
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  console.log(process.env.SESSION_SECRET);
 
   try{
-    console.log(process.env.GEMINI_API_KEY);
     const sentence = xss(req.body.sentence);
     console.log("rewrite sentence");
     console.log(sentence);
   
     // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-    console.log(model);
-    
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"}); 
     const prompt = "Rewrite following sentence:\n" + sentence;
-    console.log(prompt);
     
     const result = await model.generateContent(prompt);
-    console.log(result);
     const response = await result.response;
-    console.log(response);
     const text = response.text();
     console.log("ai rewrite sentence");
     console.log(text);
